76f47ff2da3a48b6767bb1dbb993389cd2f443d5
diyavantiya <diyavantiya@gmail.com>
2025-09-07 01:48:46 +0530

    Create DollarToRupees.c
    Convert Dollar to Rupees

---
+++ DollarToRupees.c
@@ -0,0 +1,9 @@
+// Convert dollar to rupees 
+#include <stdio.h>
+int main() {
+float dollar;
+printf("Enter dollar to rupees:");
+   scanf("%f", &dollar);
+    printf("%f dollar = %f rupees ",dollar,dollar*48);
+    return 0;
+}
