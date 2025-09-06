b81842a0c3e2e8e891556cc94d8b2e0d4b452db5
diyavantiya <diyavantiya@gmail.com>
2025-09-07 00:27:07 +0530

    Create HourToMin.c
    Converting Hours to Minutes

---
+++ HourToMin.c
@@ -0,0 +1,9 @@
+// Convert hours into mins 
+#include <stdio.h>
+int main() {
+float hours;
+printf("Enter the no of hours that need to be converted to mins:");
+   scanf("%f", &hours);
+    printf("%f hours = %f minutes ",hours,hours*60);
+    return 0;
+}
