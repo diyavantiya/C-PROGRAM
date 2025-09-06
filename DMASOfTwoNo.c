627c1e71b7f0c1012d373b99a15cf0c30374c48a
diyavantiya <diyavantiya@gmail.com>
2025-09-07 00:19:57 +0530

    Create DMASOfTwoNo.c
    Addition, Subtraction, Multiplication and Division of Same Two Numbers

---
+++ DMASOfTwoNo.c
@@ -0,0 +1,12 @@
+// Add, Subtract, Multipy and Divide two no
+#include <stdio.h>
+int main() {
+float a,b;
+printf("Enter two nos to be added, subtracted, multiplied and divided:");
+   scanf("%f %f", &a,&b);
+    printf("The additon of %f and %f is %f\n",a,b,a+b);
+    printf("The subtraction of %f and %f is %f\n",a,b,a-b);
+    printf("The multiplication of %f and %f is %f\n",a,b,a*b);
+    printf("The division of %f and %f is %f",a,b,a/b);
+    return 0;
+}
