a44cbf9bf19390254b85b576263ffc37db77722b
diyavantiya <diyavantiya@gmail.com>
2025-09-07 00:07:58 +0530

    Create MultiplyTwoNo.c
    Multiplying Two Numbers

---
+++ MultiplyTwoNo.c
@@ -0,0 +1,9 @@
+// Multiply two no
+#include <stdio.h>
+int main() {
+int a,b;
+printf("Enter two nos to be multiplied:");
+   scanf("%d %d", &a,&b);
+   printf("The multiplication of %d and %d is %d.",a,b,a*b);
+    return 0;
+}
