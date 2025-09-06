555ee0c98701fe33145933624f7327cbd88d962d
diyavantiya <diyavantiya@gmail.com>
2025-09-07 00:06:06 +0530

    Create SubTwoNo.c
    Subtracting Two numbers

---
+++ SubTwoNo.c
@@ -0,0 +1,9 @@
+// Subtract two no
+#include <stdio.h>
+int main() {
+int a,b;
+printf("Enter two nos to be subtracted:");
+   scanf("%d %d", &a,&b);
+   printf("The subtraction of %d and %d is %d.",a,b,a-b);
+    return 0;
+}
