13e70df67e38622479cdc7dfc691283a3e5ab761
diyavantiya <diyavantiya@gmail.com>
2025-09-06 22:07:25 +0530

    Create AddTwoNo.c
    Adding two numbers

---
+++ AddTwoNo.c
@@ -0,0 +1,9 @@
+  // Add two no
+#include <stdio.h>
+int main() {
+int a,b;
+printf("Enter two nos to be added:");
+   scanf("%d %d", &a,&b);
+   printf("The addition of %d and %d is %d.",a,b,a+b);
+    return 0;
+}
