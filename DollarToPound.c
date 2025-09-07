cd177c67643c46b42ec5adc705c8a888cb278585
diyavantiya <diyavantiya@gmail.com>
2025-09-07 17:38:37 +0530

    Update DollarToPound.c
    Convert Dollars into Pounds

--- DollarToPound.c
+++ DollarToPound.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main() {
 float dollar;
-printf("Enter dollar to pounds:");
+printf("Enter the price in dollars to convert into pounds:");
    scanf("%f", &dollar);
     printf("%f dollar = %f pounds ",dollar,dollar*48/70);
     return 0;
