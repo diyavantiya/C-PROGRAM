85bf3a7c87c97338631ee1964c15573f2fb98544
diyavantiya <diyavantiya@gmail.com>
2025-09-07 17:41:19 +0530

    Update GramToKg.c
    Convert Grams to KG

--- GramToKg.c
+++ GramToKg.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 int main() {
 float grams;
-printf("Enter grams to kg:");
+printf("Enter the weight in grams to convert into kg:");
    scanf("%f", &grams);
     printf("%f grams = %f kg ",grams,grams/1000);
     return 0;
