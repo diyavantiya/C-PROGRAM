36f180415e78fb8328131d75ec590180bfa72607
diyavantiya <diyavantiya@gmail.com>
2025-09-07 19:03:03 +0530

    Create Area&PeriSq.c
    Calculate Area And Perimeter of Square

---
+++ Area&PeriSq.c
@@ -0,0 +1,10 @@
+//Calculate Area and Perimeter of Square
+#include <stdio.h>
+int main() {
+float a;
+printf("Enter side of Square to calculate perimeter and area:");
+   scanf("%f", &a);
+    printf("The area of square is %f\n",a*a);
+     printf("The perimeter of square is %f",4*a);
+    return 0;
+}
