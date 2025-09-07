c19eea19eb5de46dffcd434981f38aa74b134b0f
diyavantiya <diyavantiya@gmail.com>
2025-09-07 19:20:00 +0530

    Create Avg.c
    Calculate Average of 3 Subjects

---
+++ Avg.c
@@ -0,0 +1,13 @@
+//Calculate Average of Three Subjects
+#include <stdio.h>
+int main() {
+float a,b,c;
+printf("Enter marks of Subject 1 :");
+   scanf("%f", &a);
+   printf("Enter marks of Subject 2 :");
+   scanf("%f", &b);
+   printf("Enter marks of Subject 3 :");
+   scanf("%f", &c);
+    printf("The average of three subjects is %f\n",(a+b+c)/3);
+    return 0;
+}
