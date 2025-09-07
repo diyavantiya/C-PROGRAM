7e0a45e5d2430cb646ab87680c45e19b96a49238
diyavantiya <diyavantiya@gmail.com>
2025-09-07 19:12:31 +0530

    Create AreaOfTriangle.c
    Calculate Area of triangle

---
+++ AreaOfTri.c
@@ -0,0 +1,11 @@
+//Calculate Area of Triangle
+#include <stdio.h>
+int main() {
+float b,h;
+printf("Enter base length of triangle  :");
+   scanf("%f", &b);
+   printf("Enter height of triangle  :");
+   scanf("%f", &h);
+    printf("The area of triangle is %f\n",b*h/2);
+    return 0;
+}
