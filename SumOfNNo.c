8d44ebf5c97eb795f350db70fc77ea9bebfc8c7f
diyavantiya <diyavantiya@gmail.com>
2025-09-08 07:51:50 +0530

    Create SumOfNNo.c
    Print Sum of N natural numbers

---
+++ SumOfNNo.c
@@ -0,0 +1,13 @@
+// Print sum of n natural numbers
+
+#include<stdio.h>
+int main() {
+int a;
+printf("Enter a number to find sum of n numbers:");
+scanf("%d", &a);
+for(int i=1; i<=n; i++) {
+sum=sum+i
+}
+printf("Sum of first %d natural numbers is %d",a,sum);
+return 0;
+}
