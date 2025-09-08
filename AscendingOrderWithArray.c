56f05b8947854be7e3697f014cfa7fc9f4aa320b
diyavantiya <diyavantiya@gmail.com>
2025-09-08 09:58:55 +0530

    Update AscendingOrderWithArray.c
    Accept 5 values and arrange in ascending order with array

--- AscendingOrderWithArray.c
+++ AscendingOrderWithArray.c
@@ -22,7 +22,7 @@ for(int i=0; i<4; i++)
     a[j]=c;
     }
 
-    printf("The ascending order is:");
+    printf("The ascending order is:\n");
     for(int i=0; i<5; i++){
         printf("%d\n", a[i]);}
 return 0;
