fe0190a075e2835ac23505f8566b7750fe1b59ea
diyavantiya <diyavantiya@gmail.com>
2025-09-08 07:24:15 +0530

    Create Print no.c
    Print n natural numbers

---
+++ Print no.c
@@ -0,0 +1,12 @@
+// Print n natural numbers
+
+#include<stdio.h>
+int main() {
+int a;
+printf("Enter a number to print natural numbers upto n:");
+scanf("%d", &a);
+for(int i=1; i<=n; i++) {
+printf("%d\n", i);
+}
+return 0;
+}
